45ebaaa9553f6ccc3c1365a7659142d6fc2f5837
MBarrows20 <michael.barrows.a@gmail.com>
2019-09-11 13:35:57 -0400

    altered install script to use feature branch

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ while true; do
         [Yy]* ) 
             cp -f admin/style/vendor/skin-blue.min.css admin/style/vendor/skin-blue.min.bkp.css  # Backup Skin StyleSheet...
             cp -f admin/style/vendor/daterangepicker.css admin/style/vendor/daterangepicker.bkp.css  # Backup Calendar StyleSheet...
+            cp -f admin/scripts/vendor/app.min.js admin/scripts/vendor/app.min.bkp.js # Backup app.js
             echo "Backup completed!"
 			break ;;
         [Nn]* ) break;;
@@ -16,8 +17,12 @@ while true; do
     case $yn in
         [Yy]* ) 
 	    git clone https://github.com/mbarrows20/pi-hole-material-dark.git temp
+            cd temp
+            git checkout feature_graphicColors
+            cd ..
             rm -f admin/style/vendor/skin-blue.min.css
             rm -f admin/style/vendor/daterangepicker.css
+            rm -f admin/scripts/vendor/app.min.js
             cp temp/skin-blue.min.css admin/style/vendor/skin-blue.min.css
             cp temp/daterangepicker.css admin/style/vendor/daterangepicker.css
 	    rm -rf temp/
