4a182be1b342ea8701877bc604fb6ae8f3c305cb
Michael Barrows <michael.barrows.a@gmail.com>
2019-09-11 15:03:20 -0400

    Feature graphic colors (#3)
    * new color scheme
    
    * altered install script to use feature branch
    
    * file structure changes and install update
    
    * removed under shading on queryovertimechart
    
    * changed queries over time colors slightly

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ while true; do
         [Yy]* ) 
             cp -f admin/style/vendor/skin-blue.min.css admin/style/vendor/skin-blue.min.bkp.css  # Backup Skin StyleSheet...
             cp -f admin/style/vendor/daterangepicker.css admin/style/vendor/daterangepicker.bkp.css  # Backup Calendar StyleSheet...
+            cp -f admin/scripts/vendor/app.min.js admin/scripts/vendor/app.min.bkp.js # Backup app.js
+            cp -f admin/scripts/pi-hole/js/index.js admin/scripts/pi-hole/js/index.bkp.js # Backup index.js
             echo "Backup completed!"
 			break ;;
         [Nn]* ) break;;
@@ -16,10 +18,17 @@ while true; do
     case $yn in
         [Yy]* ) 
 	    git clone https://github.com/mbarrows20/pi-hole-material-dark.git temp
+            cd temp
+            git checkout feature_graphicColors
+            cd ..
             rm -f admin/style/vendor/skin-blue.min.css
             rm -f admin/style/vendor/daterangepicker.css
-            cp temp/skin-blue.min.css admin/style/vendor/skin-blue.min.css
-            cp temp/daterangepicker.css admin/style/vendor/daterangepicker.css
+            rm -f admin/scripts/vendor/app.min.js
+            rm -f admin/scripts/pi-hole/js/index.js
+            cp temp/style/vendor/skin-blue.min.css admin/style/vendor/skin-blue.min.css
+            cp temp/style/vendor/daterangepicker.css admin/style/vendor/daterangepicker.css
+            cp temp/scripts/vendor/app.min.js admin/scripts/vendor/app.min.js
+            cp temp/scripts/pi-hole/js/index.js admin/scripts/pi-hole/js/index.js
 	    rm -rf temp/
 			break;;
         [Nn]* ) break;;
