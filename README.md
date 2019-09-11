9282038e62ecc4db7ed2ecb82e3ef307c358bd3b
MAB <mab@pop-os.localdomain>
2019-09-11 08:33:51 -0400

    groud work for an install script

--- README.md
+++ README.md
@@ -6,13 +6,22 @@ Created for use with [pi-hole](https://github.com/pi-hole/pi-hole).
 ![Screenshot](dashboard.png)
 
 ---
+## Easy install
+````
+1. open a terminal (or SSH if you're remoting in) ( <kbd>Ctrl</kbd> + <kbd>Alt</kbd> + <kbd>T</kbd> )
+2. Type: `cd /var/www/html/`
+3. Type: `sudo wget https://github.com/MBarrows20/pi-hole-material-dark/AutoInstall/install.sh`
+4. Type: `sudo chmod +x install.sh`
+5. Type: `sudo ./install.sh`
+6. Follow the onscreen instructions!
+````
 
-## Install
+## Manual Install
 Type the following commands into SSH, line by line.
 
 ```
 cd /var/www/html/admin/style/vendor/
-sudo git clone https://github.com/MBarrows20/pi-hole-material-dark.git
+sudo git clone https://github.com/MBarrows20/pi-hole-material-dark/AutoInstall.git
 sudo rm -f skin-blue.min.css
 sudo rm -f daterangepicker.css
 sudo cp pi-hole-material-dark/skin-blue.min.css .
---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+while true; do
+    read -p "Create a backup of the current files? (Y/n): " yn
+    case $yn in
+        [Yy]* ) 
+            cp -f admin/style/vendor/skin-blue.min.css admin/style/vendor/skin-blue.min.bkp.css  # Backup Skin StyleSheet...
+            cp -f admin/style/vendor/daterangepicker.css admin/style/vendor/daterangepicker.bkp.css  # Backup Calendar StyleSheet...
+            echo "Backup completed!"
+			break ;;
+        [Nn]* ) break;;
+        * ) echo "Please answer Yes (y) or No (n).";;
+    esac
+done
+while true; do
+    read -p "Install MBarrows20's Material Dark Theme now? (Y/n): " yn
+    case $yn in
+        [Yy]* ) 
+	    git clone https://github.com/mbarrows20/pi-hole-material-dark.git temp
+            rm -f admin/style/vendor/skin-blue.min.css
+            rm -f admin/style/vendor/daterangepicker.css
+            cp temp/admin/style/vendor/skin-blue.min.css admin/style/vendor/skin-blue.min.css
+            cp temp/admin/style/vendor/daterangepicker.css admin/style/vendor/daterangepicker.css
+	    rm -rf temp/
+			break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer Yes (y) or No (n).";;
+    esac
+done
+echo "All done!"
